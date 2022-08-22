560c21b5fc782d8d3bd4587b772ea15a1d37fbed
JungwooYang <euistgh@gmail.com>
2022-08-22 15:00:22 +0900

    mutation

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const typeDefs = gql`
     allTweets: [Tweet]
     tweet(id: ID): Tweet
   }
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    dlelteTweet(id: ID): Boolean
+  }
 `;
 
 const server = new ApolloServer({ typeDefs });
