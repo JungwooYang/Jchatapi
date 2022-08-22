73ba6638426c45e0a84b3abb66a3a8b465a7b835
JungwooYang <euistgh@gmail.com>
2022-08-22 11:03:32 +0900

    query type

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({}); //throw Error('Apollo Server requires either an existing schema, modules or typeDefs');
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
